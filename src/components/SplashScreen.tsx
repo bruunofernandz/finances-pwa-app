dd15b4d6c5736e99400a203d728d37b51d6c7bdf
bruunofernandz <bruunofernandz@hotmail.com>
2024-11-04 21:11:40 -0300

    feat: splash screen

---
+++ src/components/SplashScreen.tsx
@@ -0,0 +1,63 @@
+import { useEffect, useState } from 'react';
+import anime from 'animejs';
+import Image from 'next/image';
+import Logo from '../app/icon-512x512.png';
+
+export function SplashScreen({ handleIsLoaded }: any) {
+  const [isMounted, setIsMounted] = useState(false);
+
+  const animate = () => {
+    const loader = anime.timeline({
+      complete: () => handleIsLoaded(),
+    });
+
+    loader
+      .add({
+        targets: '#logo',
+        delay: 0,
+        scale: 1,
+        duration: 500,
+        easing: 'easeInOutExpo',
+      })
+      .add({
+        targets: '#logo',
+        delay: 0,
+        scale: 1.25,
+        duration: 500,
+        easing: 'easeInOutExpo',
+      })
+      .add({
+        targets: '#logo',
+        delay: 0,
+        scale: 1,
+        duration: 500,
+        easing: 'easeInOutExpo',
+      })
+      .add({
+        targets: '#logo',
+        delay: 0,
+        scale: 1.25,
+        duration: 500,
+        easing: 'easeInOutExpo',
+      });
+  };
+
+  useEffect(() => {
+    const timeout = setTimeout(() => setIsMounted(true), 100);
+    animate();
+    return () => clearTimeout(timeout);
+  }, []);
+
+  return (
+    isMounted && (
+      <div className="flex h-screen items-center justify-center bg-[#ffffff]">
+        <Image
+          id="#logo"
+          src={Logo}
+          className="h-[150px] w-[150px]"
+          alt={'Logo'}
+        />
+      </div>
+    )
+  );
+}
